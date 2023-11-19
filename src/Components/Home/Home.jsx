7d017a12f7d64f2c214c730874119376ac174e00
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2023-11-19 13:14:17 +0600

    navbar added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <h2 className='bg-blue-500 text-white p-4'>Hi there this is home</h2>
         </div>
     );
 };
